9f49655f6a21d0fe1879ab6e65c1aaf94f8434f3
IliaPopovsky <popovskyilia@gmail.com>
2024-08-13 15:18:53 +0300

    вторая версия проекта

--- seeker_zip.c
+++ seeker_zip.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#define SIZE 100
 
 int main(void)
 {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    char *source_string = (char *)calloc(SIZE, sizeof(char));
+    char *name_source = (char*)calloc(SIZE, sizeof(char));
+    FILE *fp;
+    int counter;
+    char read_symbol;
 
     printf("Hello world!\n");
     return 0;
